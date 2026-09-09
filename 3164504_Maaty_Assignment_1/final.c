4f1e2c37716890bc7b476fbf88cb5bc7dcaf2920
kmaaty <khalid.maaty@ku.edu>
2026-09-09 00:00:06 -0500

    Update final.c

---
+++ 3164504_Maaty_Assignment_1/final.c
@@ -0,0 +1,7 @@
+EECS 348 Assignment 1
+C program that picks a secret number between 1 and 10 and the user gets up to 3 tries to guess the number
+Inputs: 5, 8, 7
+Output: "Too low! Try again." and "Too low! Try again." and "Correct! You win!"
+Sources: ChatGPT
+Name: Khalid Maaty
+Date: 9/8/26
